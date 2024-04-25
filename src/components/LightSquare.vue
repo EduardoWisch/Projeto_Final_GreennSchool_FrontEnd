36b7713e6d3d03848484120a4f9e7a1bb6814611
EduardoWisch <eduardo.wisch@escola.pr.gov.br>
2024-04-25 19:15:41 -0300

    Apenas testando

---
+++ src/components/LightSquare.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<style scoped>
+    div {
+        width: 609px;
+        height: 599px;
+        top: 31px;
+        left: 31px;
+        gap: 0px;
+        opacity: 0px;
+        background-color: blue;
+    }
+
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import LightSquare from '@/components/LightSquare.vue';
+</script>
+
+<template>
+    <div class="divMae">
+        <div>
+            
+        </div>
+    </div>
+    <LightSquare />
+</template>
+
+<style scoped>
+    .divMae {
+        background-color: white
+    }
+</style>
